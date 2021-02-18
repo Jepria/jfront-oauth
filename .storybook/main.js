e02525006601a0d640c78339cbd942740e392334
AlexSav94 <aleksoider@gmail.com>
2021-02-18 15:25:29 +0300

    chore: dependency update BREAKING CHANGE: react 17.0.1

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,38 @@
 module.exports = {
-  stories: ["../packages/**/stories/*.stories.tsx"],
+  stories: ["../packages/**/stories/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "storybook-addon-performance/register",
-    "@storybook/addon-a11y/register",
+    "@storybook/addon-a11y",
+    "@storybook/addon-toolbars",
+    "@storybook/addon-storysource",
   ],
-  typescript: {
-    reactDocgen: false,
-  },
+  webpackFinal: async (config, { configType }) => ({
+    ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules.slice(1),
+        {
+          test: /\.(ts|js|jsx|tsx?)$/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                exclude: [
+                  // \\ for Windows, / for macOS and Linux
+                  /node_modules[\\/]core-js/,
+                  /node_modules[\\/]webpack[\\/]buildin/,
+                ],
+                presets: ["@babel/preset-env"],
+                plugins: ["@babel/plugin-proposal-class-properties"],
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      ...config.resolve,
+      extensions: [...(config.resolve.extensions || []), ".ts", ".tsx"],
+    },
+  }),
 }
